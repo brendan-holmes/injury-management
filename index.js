356de4f8da93fe59c13f331d08be9664bc5c47db
Brendan Holmes <brendholmes@gmail.com>
2020-09-13 12:44:34 +1000

    Removed unimplemented production code

--- index.js
+++ index.js
@@ -74,23 +74,13 @@ app.post('/mews', [
             });
 })
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+// Serve any static files
+app.use(express.static(path.join(__dirname, 'client')));
 
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-} else {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client')));
-
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client', 'index.html'));
-    });
-}
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
