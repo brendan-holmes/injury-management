6cbdd8a241ede4bec34377c19116f6bb23725ce7
Brendan Holmes <brendholmes@gmail.com>
2020-09-24 22:46:38 +1000

    Time stamp now says x seconds/minutes/hours etc ago

--- client/client.js
+++ client/client.js
@@ -11,6 +11,34 @@ const hideForm = () => form.style.display = 'none';
 const showLoadingElement = () => loadingElement.style.display = '';
 const hideLoadingElement = () => loadingElement.style.display = 'none';
 
+function timeSincePost(date) {
+    var milliseconds = Date.now() - date;
+    var seconds = milliseconds / 1000;
+    var minutes = seconds / 60;
+    var hours = minutes / 60;
+    var days = hours / 24;
+    var weeks = days / 7;
+    var years = days / 365;
+
+    if (seconds < 30) {
+        return 'Just now';
+    } else if (seconds < 60) {
+        return `${Math.round(seconds)} second${Math.round(seconds) > 1 ? 's' : ''} ago`;
+    } else if (minutes < 60) {
+        return `${Math.round(minutes)} minute${Math.round(minutes) > 1 ? 's' : ''} ago`;
+    } else if (hours < 24) {
+        return `${Math.round(hours)} hour${Math.round(hours) > 1 ? 's' : ''} ago`;
+    } else if (days < 7) {
+        return `${Math.round(days)} day${Math.round(days) > 1 ? 's' : ''} ago`;
+    } else if (weeks < 60) {
+        return `${Math.round(weeks)} week${Math.round(weeks) > 1 ? 's' : ''} ago`;
+    } else {
+        return `${Math.round(years)} year${Math.round(years) > 1 ? 's' : ''} ago`;
+    }
+
+    return 
+}
+
 function listAllMews () {
     mewsElement.innerHTML = '';
     fetch(apiUrl)
@@ -28,7 +56,7 @@ function listAllMews () {
                 content.textContent = mew.content;
 
                 const date = document.createElement('small');
-                date.textContent = new Date(mew.created);
+                date.textContent = timeSincePost(new Date(mew.created));
 
                 div.appendChild(header);
                 div.appendChild(content);
